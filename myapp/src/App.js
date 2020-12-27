e0ced82e8faac509519067daeb59d58e57808c55
Başak Acar <acarbasak4@gmail.com>
2020-12-27 18:58:25 +0300

    conditional rendering

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,66 +1,93 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-    <Greeting name="Başak" age="24"></Greeting>
-    <GreetingClass name="Class" age="4"></GreetingClass>
-    </div>
-  );
-}
-//Functional Component
-const Greeting = props => {
-  return(
-    <p> Hi there! (Functional) 
-    Your name is {props.name} and your age is {props.age}
-    </p>
-  );
-}
+// function App() {
+//   return (
+//     <div>
+//     <Greeting name="Başak" age="24"></Greeting>
+//     <GreetingClass name="Class" age="4"></GreetingClass>
+//     </div>
+//   );
+// }
+// //Functional Component
+// const Greeting = props => {
+//   return(
+//     <p> Hi there! (Functional)
+//     Your name is {props.name} and your age is {props.age}
+//     </p>
+//   );
+// }
 
-//Class Component
+// //Class Component
 
-class GreetingClass extends Component{
-constructor(props){
-  super(props);
+// class GreetingClass extends Component{
+// constructor(props){
+//   super(props);
 
-  this.state ={
-      name:"Ali",
-      age:"36"
+//   this.state ={
+//       name:"Ali",
+//       age:"36"
+//     }
+
+// }
+// ageDown = () =>{
+//   this.setState((previousState =>({
+//     age: previousState.age - 1
+//   })));
+
+// }
+// ageUp = () =>{
+//   this.setState((previousState =>({
+//     age: previousState.age + 1
+//   })));
+
+// }
+
+//   render(){
+//     return(
+//       <div>
+//       <p>Hi there! (Class)
+//       Name is {this.props.name} and age is {this.props.age}
+//       </p>
+//       <p> {this.state.name}</p>
+//       <p>{this.state.age} </p>
+//       <button onClick={this.ageUp}>Up</button>
+//       <button onClick={this.ageDown}>Down</button>
+//       </div>
+//     );
+//   }
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showButton: false,
+    };
+  }
+
+  update = () => {
+    this.setState((previousState) => ({
+      showButton: !previousState.showButton,
+    }));
+  };
+
+  render() {
+    const { showButton } = this.state;
+    if (showButton) {
+      return (
+        <div>
+          <button>My Button</button>
+          <button onClick={this.update}>Update</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.update}>Update</button>
+        </div>
+      );
     }
-  
-}
-ageDown = () =>{
-  this.setState((previousState =>({
-    age: previousState.age - 1
-  })));
-
-}
-ageUp = () =>{
-  this.setState((previousState =>({
-    age: previousState.age + 1
-  })));
-
-}
-
-  render(){
-    return(
-      <div>
-      <p>Hi there! (Class)
-      Name is {this.props.name} and age is {this.props.age}
-      </p>
-      <p> {this.state.name}</p>
-      <p>{this.state.age} </p>
-      <button onClick={this.ageUp}>Up</button>
-      <button onClick={this.ageDown}>Down</button>
-      </div>
-    );
   }
 }
-
-
-
-
-
-
 
 export default App;
