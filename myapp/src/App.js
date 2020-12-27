db7a74ef76c7b540a091fd3ccdca9ffc947cae5c
Başak Acar <acarbasak4@gmail.com>
2020-12-27 03:23:46 +0300

    props and state

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,24 +3,42 @@ import React, {Component} from 'react';
 function App() {
   return (
     <div>
-    <Greeting></Greeting>
-    <GreetingClass></GreetingClass>
+    <Greeting name="Başak" age="24"></Greeting>
+    <GreetingClass name="Class" age="4"></GreetingClass>
     </div>
   );
 }
 //Functional Component
 const Greeting = props => {
   return(
-    <p> Hi there! (Functional) </p>
+    <p> Hi there! (Functional) 
+    Your name is {props.name} and your age is {props.age}
+    </p>
   );
 }
 
 //Class Component
 
 class GreetingClass extends Component{
+constructor(props){
+  super(props);
+
+  this.state ={
+      name:"Ali",
+      age:"36"
+    }
+  
+}
+
   render(){
     return(
-      <p>Hi there! (Class)</p>
+      <div>
+      <p>Hi there! (Class)
+      Name is {this.props.name} and age is {this.props.age}
+      </p>
+      <p> {this.state.name}</p>
+      <p>{this.state.age} </p>
+      </div>
     );
   }
 }
