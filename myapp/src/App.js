e6220c5d9653bd81dbb97ac7166401b1149533bf
Başak Acar <acarbasak4@gmail.com>
2020-12-27 21:00:25 +0300

    Fetct Api

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,81 +1,45 @@
 import React, { Component } from "react";
-import One from "./One";
-import { Two, Three } from "./Two";
 
-// function App() {
-//   return (
-//     <div>
-//     <Greeting name="Başak" age="24"></Greeting>
-//     <GreetingClass name="Class" age="4"></GreetingClass>
-//     </div>
-//   );
-// }
-// //Functional Component
-// const Greeting = props => {
-//   return(
-//     <p> Hi there! (Functional)
-//     Your name is {props.name} and your age is {props.age}
-//     </p>
-//   );
-// }
-
-// //Class Component
-
-// class GreetingClass extends Component{
-// constructor(props){
-//   super(props);
-
-//   this.state ={
-//       name:"Ali",
-//       age:"36"
-//     }
-
-// }
-// ageDown = () =>{
-//   this.setState((previousState =>({
-//     age: previousState.age - 1
-//   })));
-
-// }
-// ageUp = () =>{
-//   this.setState((previousState =>({
-//     age: previousState.age + 1
-//   })));
-
-// }
-
-//   render(){
-//     return(
-//       <div>
-//       <p>Hi there! (Class)
-//       Name is {this.props.name} and age is {this.props.age}
-//       </p>
-//       <p> {this.state.name}</p>
-//       <p>{this.state.age} </p>
-//       <button onClick={this.ageUp}>Up</button>
-//       <button onClick={this.ageDown}>Down</button>
-//       </div>
-//     );
-//   }
-// }
 
 class App extends Component {
-  constructor(props) {
+
+  constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+  componentDidMount(){
+
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json=> {
+      this.setState({
+        isLoaded:true,
+        items: json,
+      })
+    });
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <One></One>
-        <Two></Two>
-        <Three></Three>
+  render() {    
+    const {isLoaded, items} = this.state;
 
-        <h1>Hello</h1>
-        <h1>Hello 2</h1>
-      </React.Fragment>
-    );
+    if(!isLoaded){
+      return <div>Loading...</div>
+    } else{
+      return (
+        <div className="App">
+        <ul>
+        {items.map(item=>(
+          <li key={item.id}>
+          Name: {item.name} | Email: {item.email}
+          </li>
+        ))}
+        </ul>
+        </div>
+      );
+    }
   }
 }
 
