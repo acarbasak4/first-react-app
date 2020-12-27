51b9ab635d690addb4ea7665e7018531ef78f663
Başak Acar <acarbasak4@gmail.com>
2020-12-27 03:01:39 +0300

    learning components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,8 +1,20 @@
 function App() {
   return (
     <div>
-    
+    <Greeting></Greeting>
     </div>
   );
 }
+
+const Greeting = props => {
+  return(
+    <p> Hello there! </p>
+  );
+}
+
+
+
+
+
+
 export default App;
